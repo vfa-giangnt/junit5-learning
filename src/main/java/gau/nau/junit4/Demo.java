15e307839258802bde5fd8c91e10cee91fc99354
vfa.giangnt <giangnt@vitalify.asia>
2020-07-24 13:51:13 +0700

    JUnit4: Test demo simple

---
+++ src/main/java/gau/nau/junit4/Demo.java
@@ -0,0 +1,12 @@
+package gau.nau.junit4;
+
+public class Demo {
+    public boolean isPrimeNumber(int input) {
+        
+        for (int i = 2; i < input; i++) {
+            if (input % i == 0)
+                return false;
+        }
+        return true;
+    }
+}
---
+++ src/test/java/gau/nau/junit4/TestDemo.java
@@ -0,0 +1,51 @@
+package gau.nau.junit4;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class TestDemo {
+    
+    @Test
+    public void testIsPrimeNumber1() {
+        Demo demo1 = new Demo();
+        boolean result = demo1.isPrimeNumber(-1);
+        assertTrue(result);
+    }
+    
+    @Test
+    public void testIsPrimeNumber2() {
+        Demo demo2 = new Demo();
+        boolean result = demo2.isPrimeNumber(0);
+        assertTrue(result);
+    }
+    
+    @Test
+    public void testIsPrimeNumber3() {
+        Demo demo2 = new Demo();
+        boolean result = demo2.isPrimeNumber(2);
+        assertTrue(result);
+    }
+    
+    @Test
+    public void testIsPrimeNumber4() {
+        Demo demo2 = new Demo();
+        boolean result = demo2.isPrimeNumber(4);
+        assertFalse(result);
+    }
+    
+    @Test
+    public void testIsPrimeNumber5() {
+        Demo demo2 = new Demo();
+        boolean result = demo2.isPrimeNumber(5);
+        assertTrue(result);
+    }
+    
+    @Test
+    public void testIsPrimeNumber6() {
+        Demo demo2 = new Demo();
+        boolean result = demo2.isPrimeNumber(6);
+        assertFalse(result);
+    }
+}
