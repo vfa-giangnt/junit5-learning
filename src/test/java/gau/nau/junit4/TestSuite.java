e06a68cbb5b46b758bd1e7b296d9ffe1d351fde1
vfa.giangnt <giangnt@vitalify.asia>
2020-07-24 15:50:13 +0700

    JUnit4: Run with testRunner and testSuite

---
+++ src/test/java/gau/nau/junit4/TestSuite.java
@@ -0,0 +1,9 @@
+package gau.nau.junit4;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({AssertTest.class, DemoTest.class})
+public class TestSuite {
+}
